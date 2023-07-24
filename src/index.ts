c878c0deec016f4d7be2009b8ec74cb8e739ba8f
Finbar <finbar@finbar.xyz>
2023-07-23 20:39:25 -0400

    works with github webhooks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { createCanvas, loadImage, registerFont } from 'canvas';
 import { join } from 'path';
 import * as express from 'express';
 import * as bp from 'body-parser';
+import axios from 'axios';
+import 'dotenv/config';
+import * as fs from 'fs';
+
+const GithubColors = require('github-colors');
 
 registerFont(join(__dirname, 'assets', 'Lato-Black.ttf'), { family: 'Lato-Bold', style: 'bold', weight: 'bold' });
 registerFont(join(__dirname, 'assets', 'Lato-Regular.ttf'), { family: 'Lato', style: 'normal', weight: 'normal' });
@@ -9,6 +14,7 @@ registerFont(join(__dirname, 'assets', 'Lato-Regular.ttf'), { family: 'Lato', st
 const app = express();
 
 const cutText = (ctx, text: string, width: number) => {
+  text = text.split('\n')[0];
   const words = text.split(' ');
   const lines = [];
   let currentLine = words[0];
@@ -53,9 +59,23 @@ app.post('/wh', bp.json(), async (req: express.Request, res: express.Response) =
     event
   }, null, 2));
 
-
   if (event === 'push') {
     const canvas = createCanvas(1000, 500);
+    const { id } = req.body.head_commit;
+
+    const data = await axios({
+      method: 'GET',
+      url: `https://api.github.com/repos/${process.env.owner}/${process.env.repository}/commits/${id}`,
+      headers: {
+        'Authorization': `token ${process.env.token}`
+      }
+    })
+      .then(res => res.data)
+      .catch(err => console.error(err));
+
+    if (!data) return;
+
+    const { commit, author, stats, files } = data;
 
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = '#FFF';
@@ -63,40 +83,51 @@ app.post('/wh', bp.json(), async (req: express.Request, res: express.Response) =
 
     ctx.fillStyle = '#586069';
     ctx.font = '25px Lato';
-    ctx.fillText('OneAndOnlyFinbar/discork', 70, 70);
+    ctx.fillText(req.body.repository.full_name, 70, 70);
 
     ctx.font = '15px Lato';
     ctx.fillText('on branch', 72, 70 + 18);
 
     ctx.fillStyle = '#47494c';
     ctx.font = '15px Lato-Bold';
-    ctx.fillText('master', 72 + ctx.measureText('on branch').width - 5, 70 + 18);
+    ctx.fillText(req.body.ref, 72 + ctx.measureText('on branch').width - 5, 70 + 18);
 
     ctx.fillStyle = '#2f363d';
     ctx.font = 'bold 40px Lato-Bold';
 
-    const lines = cutText(ctx, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', 630);
+    const lines = cutText(ctx, commit.message, 630);
     lines.forEach((line, index) => {
       ctx.fillText(line, 70, 140 + (index * 50));
     });
 
     const img = await loadImage(join(__dirname, 'assets', 'changed-file-icon.png'));
-    ctx.drawImage(img, 70, 140 + lines.length * 50, 24, 28);
+    ctx.drawImage(img, 70, 500 - 20 - 90 - 75, 24, 28);
+
+    ctx.font = '25px Lato';
+    const linesChangedString = `${stats.total} ${stats.total === 1 ? 'Line' : 'Lines'} changed`;
+    const linesChangedWidth = ctx.measureText(linesChangedString).width;
+
+    ctx.font = '25px Lato-Bold';
+    const linesAddedString = `+${stats.additions}`;
+    const linesAddedWidth = ctx.measureText(linesAddedString).width;
+
+    const linesRemovedString = `-${stats.deletions}`;
+    const linesRemovedWidth = ctx.measureText(linesRemovedString).width;
 
     ctx.fillStyle = '#6e7681';
     ctx.font = '25px Lato';
-    ctx.fillText('15 Lines changed', 70 + 24 + 10, 140 + lines.length * 50 + 25);
+    ctx.fillText(linesChangedString, 70 + 24 + 10, 500 - 20 - 90 - 75 + 25);
 
     ctx.fillStyle = '#22863a';
     ctx.font = '25px Lato-Bold';
-    ctx.fillText('+50', 70 + 24 + 10 + ctx.measureText('15 Lines changed').width, 140 + lines.length * 50 + 25);
+    ctx.fillText(linesAddedString, 70 + 24 + 10 + linesChangedWidth + 15, 500 - 20 - 90 - 75 + 25);
 
     ctx.fillStyle = '#cb2431';
-    ctx.fillText('-10', 70 + 24 + 10 + ctx.measureText('15 Lines changed').width + ctx.measureText('+50').width + 15, 140 + lines.length * 50 + 25);
+    ctx.fillText(linesRemovedString, 70 + 24 + 10 + linesChangedWidth + 15 + linesAddedWidth + 15, 500 - 20 - 90 - 75 + 25);
 
-    let additionPercentage = 80;
-    const percentageStartX = 70 + 24 + 10 + ctx.measureText('15 Lines changed').width + ctx.measureText('+50').width + 15 + ctx.measureText('-10').width + 20;
-    const percentageStartY = 140 + lines.length * 50 + 5;
+    let additionPercentage = Math.round(stats.additions / stats.total * 100);
+    const percentageStartX = 70 + 24 + 10 + linesChangedWidth + 15 + linesAddedWidth + 15 + linesRemovedWidth + 15;
+    const percentageStartY = 500 - 20 - 90 - 75 + 25 - 20;
 
     for (let i = 0; i < 5; i++) {
       if (additionPercentage >= 20) {
@@ -109,24 +140,54 @@ app.post('/wh', bp.json(), async (req: express.Request, res: express.Response) =
       }
     }
 
-    const committerSmallPFP = await loadImage(join(__dirname, 'assets', 'PFP.jpg'));
-    ctx.drawImage(roundImage(committerSmallPFP, committerSmallPFP.width), 70, 500 - 20 - 90, 50, 50);
-    ctx.drawImage(roundImage(committerSmallPFP, committerSmallPFP.width), 1000 - 20 - 150, 50, 120, 120);
+    const profilePicture = await loadImage(author.avatar_url);
+    ctx.drawImage(roundImage(profilePicture, profilePicture.width), 70, 500 - 20 - 90, 50, 50);
+    ctx.drawImage(roundImage(profilePicture, profilePicture.width), 1000 - 20 - 150, 50, 120, 120);
 
     ctx.fillStyle = 'black';
     ctx.font = '25px Lato-Bold';
-    ctx.fillText('OneAndOnlyFinbar', 70 + 50 + 15, 500 - 20 - 90 + 25 + 10);
+
+    const username = author.login;
+    const usernameWidth = ctx.measureText(username).width;
+
+    ctx.fillText(username, 70 + 50 + 15, 500 - 20 - 90 + 25 + 10);
+
+    const date = new Date(commit.author.date);
+    const dateString = `${date.toLocaleString('en-us', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`;
+    const dateStringWidth = ctx.measureText(dateString).width;
 
     ctx.fillStyle = '#586069';
     ctx.font = '25px Lato';
-    ctx.fillText('committed July 14th, 2023', 70 + 50 + 15 + ctx.measureText('OneAndOnlyFinbar').width + 40, 500 - 20 - 90 + 25 + 10);
+    ctx.fillText(`committed on ${dateString}`, 70 + 50 + 15 + usernameWidth + 10, 500 - 20 - 90 + 25 + 10);
 
     ctx.fillStyle = '#47494c';
     ctx.font = '25px Lato';
-    ctx.fillText('6246b8f', 1000 - 50 - ctx.measureText('6246b8f').width, 500 - 20 - 90 + 25 + 10);
 
-    ctx.fillStyle = '#1e77e1';
-    ctx.fillRect(0, 480, 1000, 20);
+    const commitId = commit.tree.sha.substring(0, 7);
+
+    ctx.fillText(commitId, 1000 - 50 - ctx.measureText(commitId).width, 500 - 20 - 90 + 25 + 10);
+
+    const languageLines = files.reduce((acc, file) => {
+      const language = file.filename.split('.').pop();
+      if (language) {
+        if (!acc[language]) acc[language] = 0;
+        acc[language] += file.changes;
+      }
+      return acc;
+    }, {});
+
+    const languagePercentages = Object.keys(languageLines).reduce((acc, language) => {
+      acc[language] = Math.round(languageLines[language] / stats.total * 100);
+      return acc;
+    }, {});
+
+    for (let language in languagePercentages) {
+      const width = 1000 * languagePercentages[language] / 100;
+      ctx.fillStyle = GithubColors.ext(language).color;
+      ctx.fillRect(0, 480, width, 20);
+    }
+
+    fs.writeFileSync(join(__dirname, 'test.png'), canvas.toBuffer());
   }
 
   res
